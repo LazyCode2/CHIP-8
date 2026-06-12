748053e6a7a3cdc54f7f9aa2381aefd543f1063d
LazyCode2 <21darkwarrior@gmail.com>
2026-06-12 15:40:58 +0600

    feat: addfetch and execute some instructions

--- cpu/cpu.go
+++ cpu/cpu.go
@@ -18,5 +18,42 @@ func (cpu *Chip8) Init() {
 	cpu.ProgramCounter = 0x200
 }
 
+func (cpu *Chip8) Emulate() {
+	// Fetch opcode - 2 bytes
+	cpu.Opcode = uint16(cpu.Memory[cpu.ProgramCounter]) << 8 | uint16(cpu.Memory[cpu.ProgramCounter + 1])
+
+	switch cpu.Opcode & 0xF000 {
+		case 0x0000:
+			switch cpu.Opcode & 0x00FF {
+				case 0x00E0:
+					for i := range cpu.Display {
+						cpu.Display[i] = false
+					}
+					cpu.ProgramCounter += 2
+					break
+			}
+		case 0x1000:
+			cpu.ProgramCounter = cpu.Opcode & 0x0FFF
+			break
+
+		case 0x2000:
+			cpu.Stack[cpu.StackPointer] = cpu.ProgramCounter
+			cpu.StackPointer++
+			cpu.ProgramCounter = cpu.Opcode & 0xFFF
+		
+		case 0x3000:
+			x := (cpu.Opcode & 0x0F00) >> 8
+			nn := cpu.Opcode & 0x00FF
+
+			if cpu.RegisterV[x] != uint8(nn) {
+				cpu.ProgramCounter += 4
+			}
+
+			cpu.ProgramCounter += 2
+			break
+		}
+
+}
+
 
 
